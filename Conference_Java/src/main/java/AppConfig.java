3223c9f55b220f52d01ecb1c67557eb997c40129
Tae Hui Chang <jasmine878@yahoo.com>
2024-07-07 23:12:55 -0500

    Configure Spring inside of Application

---
+++ Conference_Java/src/main/java/AppConfig.java
@@ -0,0 +1,9 @@
+import com.pluralsight.service.SpeakerService;
+import com.pluralsight.service.SpeakerServiceImpl;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class AppConfig {
+
+}
