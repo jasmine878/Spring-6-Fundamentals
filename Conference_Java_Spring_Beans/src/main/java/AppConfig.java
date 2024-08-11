ae91389ae4e847a3b9697edde92bf0e436c787ff
Tae Hui Chang <jasmine878@yahoo.com>
2024-08-11 01:18:59 -0500

    Make another copy of the project

---
+++ Conference_Java_Spring_Beans/src/main/java/AppConfig.java
@@ -0,0 +1,25 @@
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan({"com.pluralsight"})
+public class AppConfig {
+
+    /*
+    @Bean(name="speakerService")
+    @Scope(value= BeanDefinition.SCOPE_SINGLETON)
+    public SpeakerService getSpeakerService() {
+//        SpeakerServiceImpl service = new SpeakerServiceImpl(getSpeakerRepository());
+        SpeakerServiceImpl service = new SpeakerServiceImpl();
+
+//        service.setRepository(getSpeakerRepository());
+
+        return service;
+    }
+
+    @Bean(name="speakerRepository")
+    public SpeakerRepository getSpeakerRepository() {
+        return new StubSpeakerRepositoryImpl();
+    }
+    */
+}
