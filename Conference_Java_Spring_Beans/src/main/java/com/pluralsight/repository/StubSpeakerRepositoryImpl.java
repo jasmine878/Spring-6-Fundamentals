ea22fb51529da6920ef8259975016f77a3cbccc3
Tae Hui Chang <jasmine878@yahoo.com>
2024-09-01 00:38:55 -0500

    Utilize the bean using setter injection

--- Conference_Java_Spring_Beans/src/main/java/com/pluralsight/repository/StubSpeakerRepositoryImpl.java
+++ Conference_Java_Spring_Beans/src/main/java/com/pluralsight/repository/StubSpeakerRepositoryImpl.java
@@ -1,13 +1,23 @@
 package com.pluralsight.repository;
 
 import com.pluralsight.model.Speaker;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
 import java.util.ArrayList;
+import java.util.Calendar;
 import java.util.List;
 
 @Repository("speakerRepository")
 public class StubSpeakerRepositoryImpl implements SpeakerRepository {
+
+    private Calendar cal;
+
+    @Autowired
+    public void setCal(Calendar cal) {
+        this.cal = cal;
+    }
+
     @Override
     public List<Speaker> findAll() {
         List<Speaker> speakers = new ArrayList<>();
@@ -16,6 +26,8 @@ public class StubSpeakerRepositoryImpl implements SpeakerRepository {
 
         speaker.setFirstName("Big");
         speaker.setLastName("Bird");
+
+        System.out.println("cal: " + cal.getTime());
 
         speakers.add(speaker);
 
